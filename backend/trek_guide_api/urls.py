e4db80ae7bba5c534eb983cb7f9397404e5ba764
Es1er <24127407@student.hcmus.edu.vn>
2025-11-16 15:28:33 +0700

    Init

--- backend/trek_guide_api/urls.py
+++ backend/trek_guide_api/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     # --- Add these two lines for JWT Authentication ---
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/users/', include('users.urls')),
     path('', home),
 ]
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'screens/trip_info_screen.dart'; // <-- Đã import
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Trek Guide',
+      // ...
+      home: const TripInfoScreen(), // <-- Đã đặt làm home
     );
   }
 }
